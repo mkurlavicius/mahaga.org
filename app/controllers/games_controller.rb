3a80315c87fda59007f189ce46d5cb133063dbfb
Mindaugas Kurlavičius <mkurlavicius@gmail.com>
2019-07-19 18:21:44 +0200

    Random message

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -17,13 +17,27 @@ class GamesController < ApplicationController
       format.json()
     end
   end
+
+  # POST /games/{game_id}/matches/{match_id}/moves.json
+  def update
+    @game = current_user.games.find(params[:id])
+    respond_to do |format|
+      format.json do
+        if @game.update(game_params)
+          render :show, status: :created, location: game_path(@game)
+        else
+          render json: @game.errors, status: :unprocessable_entity 
+        end
+      end
+    end
+  end
   
   private
 
     # Never trust parameters from the scary internet, only allow the white list through.
 
     def game_params
-      params.fetch(:game, {}).permit(:name, :label, :description)
+      params.fetch(:game, {}).permit(:name, :label, :description, :picture)
     end
   
   
